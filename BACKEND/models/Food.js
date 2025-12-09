0164d6851a8d4148c185b9b3aa4795fc4afd1219
Ritheeshselva <ritheeshselva2005@gmail.com>
2025-12-09 22:37:33 +0530

    Simple-food-program

--- BACKEND/models/Food.js
+++ BACKEND/models/Food.js
@@ -1,2 +1,33 @@
 const express = require('express');
-const mongoose = require(mongoose)
+const app = express();
+require('../db'); 
+port = 5000;
+const mongoose = require('mongoose');
+
+//schema
+const schema = new mongoose.Schema({
+    Fname:{type:String,required:true},
+    orderId:{type:Number,require:true},
+    Cust_Name:{type:String,required:true}       
+});
+
+//Model
+const food = new mongoose.model('food',schema);
+
+//input
+const newfood = new food({
+    Fname:'Biryani',
+    orderId:101,
+    Cust_Name:'Ritheesh'
+}) 
+
+//save
+
+newfood.save().then(()=>{
+    console.log("Food_Saved");
+}).catch((err)=>{
+    console.log("Error",err);
+})
+
+
+
